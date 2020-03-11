0c1b74bc02397f7b603fdbaac831193e92785400
asiagromasia <35482401+asiagromasia@users.noreply.github.com>
2020-03-10 20:28:58 -0700

    backend
    run with node

---
+++ index.js
@@ -0,0 +1,5 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import Main from './Components/Main';
+//import Reservations from './Components/Reservation';
+ReactDOM.render(<Main/>, document.getElementById('root'));
